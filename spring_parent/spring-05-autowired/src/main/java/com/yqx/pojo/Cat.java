332f73fdb7e72e83f0b86d0658010f345259ba3c
fanzx <fanzx>
2021-08-13 14:30:10 +0800

    Autowired和Resource自动装配;

---
+++ spring_parent/spring-05-autowired/src/main/java/com/yqx/pojo/Cat.java
@@ -0,0 +1,16 @@
+package com.yqx.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author fanzx
+ * @create 2021/8/13 11:16
+ */
+public class Cat {
+    public void shout(){
+        System.out.println("miao~");
+    }
+}

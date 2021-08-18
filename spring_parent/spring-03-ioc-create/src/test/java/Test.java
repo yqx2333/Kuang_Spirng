7392dcfca3fcd55dc86e557ccc9386e7771f9255
fanzx <fanzx>
2021-08-18 10:24:52 +0800

    声明式事务->环境搭建

--- spring_parent/spring-03-ioc-create/src/test/java/Test.java
+++ spring_parent/spring-03-ioc-create/src/test/java/Test.java
@@ -11,7 +11,7 @@ public class Test {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        User user = (User) context.getBean("user");
+        User user = (User) context.getBean("userAlias");
         user.show();
 
     }
